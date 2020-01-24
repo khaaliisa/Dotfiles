463870ed22c2250444bbc2e595e57fdf08d12de0
khaaliisa <duong@spycloud.com>
2020-01-24 16:07:23 -0600

    Add try catch for quote of the day

--- quoteoftheday.py
+++ quoteoftheday.py
@@ -6,6 +6,9 @@ url = "http://quotes.rest/qod.json?category=inspire"
 response = requests.request("GET", url)
 
 parser = response.json()
-quote = parser["contents"]["quotes"][0]["quote"]
-author = parser["contents"]["quotes"][0]["author"]
-print(quote, '-', author)
+try:
+    quote = parser["contents"]["quotes"][0]["quote"]
+    author = parser["contents"]["quotes"][0]["author"]
+    print(quote, '-', author)
+except:
+    print("Absorb what is useful, discard what is useless, and add what is specifically your own. - Bruce Lee")
